2a5c47e0e2e24a229f14a194dad3c5e931887317
Maria Baeta <m.martinsdg@gmail.com>
2021-06-22 16:05:22 -0300

    Exercise-one feito

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,59 +1,57 @@
 // src/Cars.jsx
 
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import CarsContext from './context/CarsContext';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
 
-class Cars extends Component {
-  render() {
-    const { redCar, blueCar, yellowCar } = this.context.cars;
-    const { moveCar } = this.context;
-    return (
+const Cars = () => {
+  const { cars, moveCar } = useContext(CarsContext);
+  const { red, blue, yellow } = cars;
+   return (
+    <div>
       <div>
-        <div>
-          <img
-            className={redCar ? 'car-right' : 'car-left'}
-            src={carRed}
-            alt="red car"
-          />
-          <button
-            onClick={() => moveCar('redCar', !redCar)}
-            type="button"
-          >
-            Move
+        <img
+          className={red ? 'car-right' : 'car-left'}
+          src={carRed}
+          alt="red car"
+        />
+        <button
+          onClick={() => moveCar('red', !red)}
+          type="button"
+        >
+          Move
         </button>
-        </div>
-        <div>
-          <img
-            className={blueCar ? 'car-right' : 'car-left'}
-            src={carBlue}
-            alt="blue car"
-          />
-          <button
-            onClick={() => moveCar('blueCar', !blueCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
-        <div>
-          <img
-            className={yellowCar ? 'car-right' : 'car-left'}
-            src={carYellow}
-            alt="yellow car"
-          />
-          <button
-            onClick={() => moveCar('yellowCar', !yellowCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
       </div>
-    )
-  }
+      <div>
+        <img
+          className={blue ? 'car-right' : 'car-left'}
+          src={carBlue}
+          alt="blue car"
+        />
+        <button
+          onClick={() => moveCar('blue', !blue)}
+          type="button"
+        >
+          Move
+        </button>
+      </div>
+      <div>
+        <img
+          className={yellow ? 'car-right' : 'car-left'}
+          src={carYellow}
+          alt="yellow car"
+        />
+        <button
+          onClick={() => moveCar('yellow', !yellow)}
+          type="button"
+        >
+          Move
+        </button>
+      </div>
+    </div>
+  )
 };
 
 Cars.contextType = CarsContext;
